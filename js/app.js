901f22d622a01feb41d197cf9d26bb44c04f5ca2
Snobeard <fatmattmatt@gmail.com>
2017-10-09 15:28:25 -0700

    added constructor with array for name and style

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 'use strict';
+
+
+// +++++++++++++++++++++++++++++++++++++++++
+// +++++++ Constructor
+// +++++++++++++++++++++++++++++++++++++++++
+function Workouts(name, length, intensity, style){
+  this.name = [];
+  this.length = length;
+  this.intensity = intensity;
+  this.style = [];
+  this.name = this.name.concat(name);
+  this.style = this.style.concat(style);
+}
