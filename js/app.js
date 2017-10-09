f3e9783e3e1ea880bf6f3094cd23765d535bfc34
Snobeard <fatmattmatt@gmail.com>
2017-10-09 15:53:30 -0700

    changed constructor

--- js/app.js
+++ js/app.js
@@ -6,11 +6,9 @@
 // +++++++++++++++++++++++++++++++++++++++++
 function Workouts(name, length, intensity, style){
   this.name = name;
-  this.length = [];
+  this.length = length;
   this.intensity = intensity;
-  this.style = [];
-  this.length = this.length.concat(length);
-  this.style = this.style.concat(style);
+  this.style = style;
 }
 
 var workout1 = new Workouts(name, [length], intensity, [style]);
