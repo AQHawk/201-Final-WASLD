1a472ac06daf922c63178deaaf0162fafe2217b7
Nick Gibson <Nicholas.a.Gibson@Gmail.com>
2017-10-11 16:36:36 -0700

    output JS ready, input objects need finalized HTML to insert

---
+++ js/output.js
@@ -0,0 +1,15 @@
+'use strict';
+
+if (localStorage.workout){
+  var workoutChoice = JSON.parse(localStorage.workout);
+} else {
+  window.location = 'inputForm.html';
+};
+
+function renderWorkout(){
+  var renderResults = document.getElementById('results').innerHTML;
+  renderResults = workoutChoice.textImgForRendering;
+  document.getElementById('results').innerHTML = renderResults;
+}
+
+renderWorkout();
