5429f9998878b71f4d38b025a3f94b51f72124f3
Snobeard <fatmattmatt@gmail.com>
2017-10-11 23:23:23 -0700

    added journal to index.html... and other things

--- js/index.js
+++ js/index.js
@@ -1,7 +1,73 @@
 'use strict';
 
 var welcome = document.getElementById('welcome');
+var welcomeHeader = document.getElementById('welcomeHeader');
+var pastJournals = document.getElementById('journalEntriesGoHere');
+NewJournal.all = [];
+
 
 if (localStorage.workout) {
   welcome.innerHTML = 'Hey ' + (JSON.parse(localStorage.workout).userName) + '! Welcome Back.';
+}
+if (JSON.parse(localStorage.journals)[0]) {
+  pastJournals.style.maxHeight = '500px';
+  pastJournals.style.overflowY = 'scroll';
+  var journalParsed = JSON.parse(localStorage.journals);
+  NewJournal.all = NewJournal.all.concat(journalParsed);
+  constructJournals();
+}
+
+
+// +++++++++++++++++++++++++++++++++++++++++
+// +++++++ Constructor
+// +++++++++++++++++++++++++++++++++++++++++
+function NewJournal(subject, textArea, id, currentWorkout){
+  this.subject = subject;
+  this.textArea = textArea;
+  this.id = id;
+  this.workoutHeader = currentWorkout;
+  NewJournal.all.push(this);
+}
+
+// +++++++++++++++++++++++++++++++++++++++++
+// +++++++ functions
+// +++++++++++++++++++++++++++++++++++++++++
+function buildJournals(subject, textArea, i, currentWorkout){
+  var secEl = document.createElement('section');
+  var header = document.createElement('h6');
+  var workout = document.createElement('h5');
+  var pEl = document.createElement('p');
+  var delEl = document.createElement('a');
+  header.textContent = (subject);
+  workout.textContent = (currentWorkout);
+  pEl.textContent = (textArea);
+  delEl.innerHTML = '<img src="img/delete.png"/>';
+  secEl.setAttribute('id', i);
+  delEl.setAttribute('onclick', 'deleteJournal(this.parentElement)');
+  secEl.appendChild(header);
+  secEl.appendChild(pEl);
+  secEl.appendChild(delEl);
+  secEl.appendChild(workout);
+  pastJournals.insertBefore(secEl, pastJournals.childNodes[0]);
+  console.log(delEl.parentElement);
+}
+
+function constructJournals() {
+  pastJournals.innerHTML = '';
+  for (var i = 0; i < NewJournal.all.length; i++){
+    buildJournals(NewJournal.all[i].subject, NewJournal.all[i].textArea, NewJournal.all[i].id, NewJournal.all[i].workoutHeader);
+  }
+  var hereAre = document.createElement('h2');
+  hereAre.textContent = 'Here are your past journals';
+  welcomeHeader.insertBefore(hereAre, welcomeHeader.childNodes[2]);
+}
+
+function deleteJournal(parent) { // eslint-disable-line
+  parent.remove();
+  NewJournal.all.splice(parent.id, 1);
+  for (var i = 0; i < (JSON.parse(localStorage.journals).length - 1); i ++) {
+    NewJournal.all[i].id = i;
+  }
+  localStorage.journals = JSON.stringify(NewJournal.all);
+  console.log(parent.id);
 }
--- js/journal.js
+++ js/journal.js
@@ -25,22 +25,25 @@ if (localStorage.workout) {
 // +++++++++++++++++++++++++++++++++++++++++
 // +++++++ Constructor
 // +++++++++++++++++++++++++++++++++++++++++
-function NewJournal(subject, textArea, id){
+function NewJournal(subject, textArea, id, currentWorkout){
   this.subject = subject;
   this.textArea = textArea;
   this.id = id;
+  this.workoutHeader = currentWorkout;
   NewJournal.all.push(this);
 }
 
 // +++++++++++++++++++++++++++++++++++++++++
 // +++++++ functions
 // +++++++++++++++++++++++++++++++++++++++++
-function buildJournals(subject, textArea, i){
+function buildJournals(subject, textArea, i, currentWorkout){
   var secEl = document.createElement('section');
   var header = document.createElement('h6');
+  var workout = document.createElement('h5');
   var pEl = document.createElement('p');
   var delEl = document.createElement('a');
   header.textContent = (subject);
+  workout.textContent = (currentWorkout);
   pEl.textContent = (textArea);
   delEl.innerHTML = '<img src="img/delete.png"/>';
   secEl.setAttribute('id', i);
@@ -48,26 +51,29 @@ function buildJournals(subject, textArea, i){
   secEl.appendChild(header);
   secEl.appendChild(pEl);
   secEl.appendChild(delEl);
+  secEl.appendChild(workout);
   pastJournals.insertBefore(secEl, pastJournals.childNodes[0]);
   console.log(delEl.parentElement);
 }
 
 function constructJournals() {
   for (var i = 0; i < NewJournal.all.length; i++){
-    buildJournals(NewJournal.all[i].subject, NewJournal.all[i].textArea, NewJournal.all[i].id);
+    buildJournals(NewJournal.all[i].subject, NewJournal.all[i].textArea, NewJournal.all[i].id, NewJournal.all[i].workoutHeader);
   }
 }
 
 function submitJournal(event){
   event.preventDefault();
+  var currentWorkout = (JSON.parse(localStorage.workout).name);
+
   if (localStorage.journals) {
     idCounter = (JSON.parse(localStorage.journals).length);
   }
   var subject = event.target.subject.value;
   var entry = event.target.entry.value;
   console.log(idCounter);
-  new NewJournal(subject, entry, idCounter);
-  buildJournals(subject, entry, idCounter);
+  new NewJournal(subject, entry, idCounter, currentWorkout);
+  buildJournals(subject, entry, idCounter, currentWorkout);
   localStorage.journals = JSON.stringify(NewJournal.all);
   event.target.subject.value = null;
   event.target.entry.value = null;
@@ -78,7 +84,7 @@ function changeJournalHeader() {
   title.textContent = (JSON.parse(localStorage.workout).name);
 }
 
-function deleteJournal(parent) {
+function deleteJournal(parent) { // eslint-disable-line
   parent.remove();
   NewJournal.all.splice(parent.id, 1);
   for (var i = 0; i < (JSON.parse(localStorage.journals).length - 1); i ++) {
